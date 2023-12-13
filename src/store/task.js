5897f92046e5bc132efdc464d32302eaed0ace87
Ronaldo Feitosa <ronaldo.feitosa@mercadolivre.com>
2023-12-13 08:07:07 -0300

    add local storage

--- src/store/task.js
+++ src/store/task.js
@@ -26,6 +26,7 @@ export const useTaskStore = defineStore('task', {
         title: this.titleTaskCreating
       })
       this.titleTaskCreating = ""
+      this.saveLocalData()
     },
     toggleDelete(index) {
       this.showDialogDelete = !this.showDialogDelete
@@ -34,10 +35,15 @@ export const useTaskStore = defineStore('task', {
     deleteTask() {
       this.tasks.splice(this.indexTaskSelected, 1)
       this.toggleDelete()
+      this.saveLocalData()
     },
     toggleEdit(index) {
       this.showDialogTaskFields = !this.showDialogTaskFields
       if (index != null) this.indexTaskSelected = index
+      this.saveLocalData()
+    },
+    saveLocalData() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
   },
 })
