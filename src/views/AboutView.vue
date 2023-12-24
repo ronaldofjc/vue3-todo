39408c7e9ef88becb98f563a6533f690305cdb13
Ronaldo Feitosa <ronaldofjc@hotmail.com>
2023-12-24 14:07:38 -0300

    add about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,24 +1,7 @@
 <template>
-  <div>
-    <v-card
-      class="mx-auto my-8"
-      max-width="800"
-      min-width="400"      
-      elevation="16"
-    >
-      <v-card-item>
-        <v-card-title>
-          Projeto Lista de Tarefas
-        </v-card-title>
-        <v-card-subtitle>
-          Aplicacao Single Page Application
-        </v-card-subtitle>
-      </v-card-item>
-
-      <v-card-text>
-        Este projeto apresenta uma forma de criar uma aplicação Frontend com Vue e Vuetify 3.
-        Tecnologias utilizadas: Vue 3, Vuetify 3, Pinia, Vue Router, AnimateCSS, Vite
-      </v-card-text>
-    </v-card>
-  </div>
+  <About/>
 </template>
+
+<script setup>
+  import About from '@/components/About.vue'
+</script>
